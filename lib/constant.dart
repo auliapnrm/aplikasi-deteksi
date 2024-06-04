8c16a6f238913a14768dc4a24ed14b652e532b92
Iqbal Fauzi <iqbalfauzien@gmail.com>
2024-06-05 03:33:37 +0700

    adding 'constant constructor'

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-Color kblue = Color(0xFF4756DF);
-Color kwhite = Color(0xFFFFFFFF);
-Color kblack = Color(0xFF000000);
-Color kbrown300 = Color(0xFF8D6E63);
-Color kbrown = Color(0xFF795548);
-Color kgrey = Color(0xFFC0C0C0);
+Color kblue = const Color(0xFF4756DF);
+Color kwhite = const Color(0xFFFFFFFF);
+Color kblack = const Color(0xFF000000);
+Color kbrown300 = const Color(0xFF8D6E63);
+Color kbrown = const Color(0xFF795548);
+Color kgrey = const Color(0xFFC0C0C0);
 
 const kBackgroundColor = Color(0xFFFEFEFE);
 const kTitleTextColor = Color(0xFF303030);
@@ -15,8 +15,8 @@ const kInfectedColor = Color(0xFFFF8748);
 const kDeathColor = Color(0xFFFF4848);
 const kRecovercolor = Color(0xFF36C12C);
 const kPrimaryColor = Color(0xFF3382CC);
-final kShadowColor = Color(0xFFB7B7B7).withOpacity(.16);
-final kActiveShadowColor = Color(0xFF4056C6).withOpacity(.15);
+final kShadowColor = const Color(0xFFB7B7B7).withOpacity(.16);
+final kActiveShadowColor = const Color(0xFF4056C6).withOpacity(.15);
 
 // Gaya Style
 const kHeadingTextStyle = TextStyle(
