fe04153aa0b39385fa5a178a819c0de1e810572c
Iqbal Fauzi <iqbalfauzien@gmail.com>
2024-06-05 04:25:00 +0700

    flask

--- android/app/src/main/kotlin/com/example/beras_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.beras_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    SECRET_KEY = 'faa4cb293448211d19d52aa5b738954dbb755f3ce1fb2ba3a2af7a011f604c03'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'db_beras'
