0712e3161112d3337926f069ae913117b9cb0023
Iqbal Fauzi <iqbalfauzien@gmail.com>
2024-06-05 16:31:54 +0700

    feat: changes title material

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Unhusked Rice Detection App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
